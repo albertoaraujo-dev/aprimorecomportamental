7acdc8185dd069a8b0d401c028e0413978f455c2
Alberto Araujo <albertoaraujo.dev@gmail.com>
2025-09-30 11:56:47 -0300

    refactor: cleaning repository

--- src/consts.ts
+++
@@ -1,9 +0,0 @@
-export const NAVBAR_HEIGHT = 60 // px
-
-export enum Section {
-  Title = 'Aprimore',
-  AboutMe = 'Sobre mim',
-  Skills = 'Especialidades',
-}
-
-export enum Tech {}
--- src/react-app-env.d.ts
+++
@@ -1,2 +0,0 @@
-//criado automáticamente
-/// <reference types="react-scripts" />
